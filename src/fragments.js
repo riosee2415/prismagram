93609968721acb14b67db449e381590f9d7dde2f
4leaf.ysh <4leaf.ysh@gmail.com>
2019-12-16 12:51:41 +0900

    create huge fragment for post

--- src/fragments.js
+++ src/fragments.js
@@ -1,24 +1,55 @@
+// export const USER_FRAGMENT = `
+//     fragment userParts on User {
+//         id
+//         userName
+//         email
+//         firstName
+//         lastName
+//         bio
+//         posts {
+//             id
+//             caption
+//         }
+//     }
+// `;
 export const USER_FRAGMENT = `
-    fragment userParts on User {
+    
         id
         userName
-        email
-        firstName
-        lastName
-        bio
-        posts {
-            id
-            caption
-        }
-    }
+    
 `;
 
 export const COMMENT_FRAGMENT = `
-    fragment CommentParts on Comment{
+    
         id
         text
         user {
-            userName
+            ${USER_FRAGMENT}
         }
+    
+`;
+
+export const FILE_FRAGMENT = `
+    
+        id
+        url
+    
+`;
+
+export const FULL_POST_FRAGMENT = `
+    fragment PostParts on Post {
+        id
+        location
+        caption
+        files {
+            ${FILE_FRAGMENT}
+        }
+        comments {
+            ${COMMENT_FRAGMENT}
+        }
+        user {
+            ${USER_FRAGMENT}
+        }
+
     }
 `;
