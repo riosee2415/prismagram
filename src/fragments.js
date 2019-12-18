c3d4fd22b477b4d1a4601a6ae6adf74e77e6d0c1
4leaf.ysh <4leaf.ysh@gmail.com>
2019-12-18 13:10:26 +0900

    refactor source

--- src/fragments.js
+++ src/fragments.js
@@ -37,24 +37,6 @@ export const FILE_FRAGMENT = `
     
 `;
 
-export const FULL_POST_FRAGMENT = `
-    fragment PostParts on Post {
-        id
-        location
-        caption
-        files {
-            ${FILE_FRAGMENT}
-        }
-        comments {
-            ${COMMENT_FRAGMENT}
-        }
-        user {
-            ${USER_FRAGMENT}
-        }
-
-    }
-`;
-
 export const MESSAGE_FRAGMENT = `
     id
     text
